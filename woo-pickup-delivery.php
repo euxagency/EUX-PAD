7c0a1367a0b3d2067d41bfee02b33fe95fe7f192
soumitra123 <email24by7@gmail.com>
2026-04-30 09:36:40 +0530

    Update woo-pickup-delivery.php
    v change

--- woo-pickup-delivery.php
+++ woo-pickup-delivery.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin constants
-define( 'WPD_VERSION', '1.0.1' );
+define( 'WPD_VERSION', '1.0.2' );
 define( 'WPD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WPD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'WPD_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
